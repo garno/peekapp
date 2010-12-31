f1e978fd5f59164e1f9358e5f47a68b36f473a26
Samuel Garneau <samgarneau@gmail.com>
2010-12-30 23:30:58 -0500

    Add Peekapp::Apps::search

--- lib/peekapp/apps.rb
+++ lib/peekapp/apps.rb
@@ -2,11 +2,13 @@ module Peekapp
 
   module Apps
 
-    def self.search query, options = {} # {{{
+    def self.search query # {{{
+      result = JSON.parse(Peekapp::query(:url => $peekapp_config[:search_url], :keywords => query.gsub(' ', '%20')))
+      result['results'].map { |a| App.new a }
     end # }}}
 
     def self.find id # {{{
-      result = JSON.parse(Peekapp::query :url => $peekapp_config[:app_url], :app_id => id)
+      result = JSON.parse(Peekapp::query :url => $peekapp_config[:app_url], :app_id => id, :store_id => "143455-5,12")
       raise AppNotFound if result["resultCount"] < 1
       result["results"].map{|a| App.new a}.first
     end # }}}
@@ -29,12 +31,16 @@ module Peekapp
 
     def initialize data # {{{
       @data = data
-    end
+    end # }}}
 
     def id # {{{
       @data["trackId"]
     end # }}}
-    # }}}
+
+    def method_missing method # {{{
+      @data[method.to_s]
+    end # }}}
+
   end
 
 end
